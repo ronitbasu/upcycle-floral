7d14ff2d67d192d26b774b77154f0ca16e48d758
Charlie Collar <charlescollar2020@u.northwestern.edu>
2019-02-19 17:36:53 -0600

    Changed flower display, added content to expand details, added nonfunctional buttons add to calendar and get directions

--- upcycle-floral/src/components/Confirmation/index.js
+++ upcycle-floral/src/components/Confirmation/index.js
@@ -9,11 +9,15 @@ class Confirmation extends Component {
         <h2>Your Order</h2>
         <img src={staticImage} />
         <p className="instructions">
-          Lilies will be available for pickup 4/21 between 10:00am and 12:00pm.
+          "Birthday Blooms" will be available for pickup 4/21 between 10:00am and 12:00pm.
         </p>
         <p className="saved">
-          You saved $350.00!
+          You saved $35.00!
         </p>
+        <div className="helperbuttons">
+          <AddToCalendar />
+          <GetDirections />
+        </div>
         <SeeReservation />
         <SearchMore />
         <Cancel />
@@ -37,9 +41,25 @@ class SeeReservation extends Component {
     return (
       <div className="seereservation">
         <hr />
-        <span onClick={() => {this.toggle();}}>{this.state.minimize?"▶":"▼"} See Details</span>
+        <span className="expander" onClick={() => {this.toggle();}}>{this.state.minimize?"▶ Expand":"▼ Hide"} Details</span>
         <div className={this.state.minimize?"hidden":"viewing"}>
-        	hellohellohello!
+          <table>
+            <tbody>
+              <tr>
+                <td>Number of Flowers:</td>
+                <td>50</td>
+              </tr>
+              <tr>
+                <td>Original value:</td>
+                <td>$49.99</td>
+              </tr>
+              <tr>
+                <td>Original Date of Arrangement:</td>
+                <td>2/16/2019</td>
+              </tr>
+            </tbody>
+          </table>
+          <span className="description">This bright arrangement is perfect as a great party centerpiece or to send to a loved one far away.</span>
         </div>
       </div>
     )
@@ -49,7 +69,7 @@ class SeeReservation extends Component {
 class SearchMore extends Component {
   render() {
     return (
-      <div className="button searchmore">Search More Arrangements</div>
+      <div className="button searchmore">Back to Search</div>
     )
   }
 }
@@ -58,6 +78,20 @@ class Cancel extends Component {
   render() {
     return (
       <div className="button cancel">Cancel</div>
+    )
+  }
+}
+class AddToCalendar extends Component {
+  render() {
+    return (
+      <button>Add to Calendar</button>
+    )
+  }
+}
+class GetDirections extends Component {
+  render() {
+    return (
+      <button>Get Directions</button>
     )
   }
 }
