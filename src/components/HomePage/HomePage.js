66e17ede5309cced2a243a87418c9c46166adf38
Charlie Collar <charlescollar2020@u.northwestern.edu>
2019-03-04 14:28:08 -0600

    Added capabilities of showing shelf from an array of the items in the catalog

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -72,6 +72,9 @@ class Listing extends Component {
   render() {
     return (
       <div className="listing">
+        <h2>{this.props.data.title}</h2>
+        <img src={require('../../static/images/'+this.props.data.image)} /><br />
+        <p>{this.props.data.description}</p>
       </div>
     );
   }
